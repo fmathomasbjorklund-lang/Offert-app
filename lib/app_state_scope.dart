3eaa20093cdede9b629560ac538c4937b9bb6d58
Thomas Björklund <fmathomasbjorklund@gmail.com>
2026-02-28 19:50:58 +0200

    order/analyze-fix

---
+++ lib/app_state_scope.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+import 'state/app_state.dart';
+
+class AppStateScope extends InheritedNotifier<AppState> {
+  const AppStateScope({
+    super.key,
+    required AppState notifier,
+    required Widget child,
+  }) : super(notifier: notifier, child: child);
+
+  static AppState of(BuildContext context) {
+    final scope =
+        context.dependOnInheritedWidgetOfExactType<AppStateScope>();
+    assert(scope != null, 'AppStateScope not found in widget tree');
+    return scope!.notifier!;
+  }
+}
